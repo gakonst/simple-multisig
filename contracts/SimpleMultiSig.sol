299da2d93384208851ba31c19bc885b499d60eff
Georgios Konstantopoulos <me@gakonst.com>
2020-09-29 12:29:02 +0300

    refactor out the checkAuth call

--- contracts/SimpleMultiSig.sol
+++ contracts/SimpleMultiSig.sol
@@ -34,25 +34,12 @@ contract SimpleMultiSig {
       address executor, 
       uint gasLimit
   ) public {
-    require(sigR.length >= threshold, "not enough signatures");
-    uint length = sigR.length;
-    require(length == sigS.length && sigR.length == sigV.length);
     require(executor == msg.sender || executor == address(0));
-
     bytes32 hash = keccak256(abi.encodePacked(
         gasLimit, destination, value, keccak256(data), address(this), executor, nonce++
     ));
-    // TODO: Re-enable this
-    // hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-    // console.logBytes32(hash);
 
-    address lastAdd = address(0); // cannot have address(0) as an owner
-    for (uint i = 0; i < length; i++) {
-      address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
-      require(recovered > lastAdd, "sender not in increasing order");
-      require(isOwner[recovered], "recovered sender is not an owner");
-      lastAdd = recovered;
-    }
+    checkAuthorized(sigV, sigR, sigS, hash);
 
     // If we make it here all signatures are accounted for.
     // The address.call() syntax is no longer recommended, see:
@@ -60,6 +47,28 @@ contract SimpleMultiSig {
     bool success = false;
     assembly { success := call(gasLimit, destination, value, add(data, 0x20), mload(data), 0, 0) }
     require(success);
+  }
+
+  // enforces the multisig requirements
+  function checkAuthorized(
+      uint8[] memory sigV, 
+      bytes32[] memory sigR, 
+      bytes32[] memory sigS, 
+      bytes32 hash
+  ) private view {
+    require(sigR.length >= threshold, "not enough signatures");
+    require(sigR.length == sigS.length && sigR.length == sigV.length, "sig arrays lengths do not match");
+    // TODO: Re-enable this
+    // hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    // console.logBytes32(hash);
+    address lastAdd = address(0); // cannot have address(0) as an owner
+    uint length = sigV.length;
+    for (uint i = 0; i < length; i++) {
+      address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+      require(recovered > lastAdd, "sender not in increasing order");
+      require(isOwner[recovered], "recovered sender is not an owner");
+      lastAdd = recovered;
+    }
   }
 
   receive () payable external {}
